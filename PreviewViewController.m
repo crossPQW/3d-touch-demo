747e284e7a7bb0f67213151412fe85416603628c
crossPQW <651024752@qq.com>
2015-10-26 23:04:27 +0800

    demo finished

--- PreviewViewController.m
+++ PreviewViewController.m
@@ -29,11 +29,9 @@
     UIPreviewAction *action1 = [UIPreviewAction actionWithTitle:@"action1" style:UIPreviewActionStyleDefault handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
         NSLog(@"action1 click");
     }];
-    
     UIPreviewAction *action2 = [UIPreviewAction actionWithTitle:@"action2" style:UIPreviewActionStyleDestructive handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
         NSLog(@"action2 click");
     }];
-    
     UIPreviewAction *action3 = [UIPreviewAction actionWithTitle:@"action3" style:UIPreviewActionStyleSelected handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
         NSLog(@"action3 click");
     }];
